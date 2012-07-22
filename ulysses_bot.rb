fa99e129083dd839d20d515e55cff385bc7874b4
Matt Waldron <mattwaldron@gmail.com>
2012-07-22 17:43:55 -0400

    Both bots now spit out their respective texts with the same shared functions

--- ulysses_bot.rb
+++ ulysses_bot.rb
@@ -6,43 +6,63 @@ class LitBot
   end
 
   def speak_first_10_words
-    puts @source_text.split(' ').first(10)
+    puts
+    print @source_text.split(' ').first(10).join(' ')
+    puts
+    puts '=' * 90 # This creates a bottom border under the text
+    puts
   end
 
   def digest_file
     source_text_words = @source_text.split(' ')
-    word_pairs_and_probabilities = {}
+    @word_pairs_and_probabilities = {}
 
     source_text_words.each_with_index do |word, index|
       hash_key = "#{word} #{source_text_words[index + 1]}"
       hash_value = source_text_words[index + 2]
-      if word_pairs_and_probabilities[hash_key]
-        word_pairs_and_probabilities[hash_key] << hash_value
+      if @word_pairs_and_probabilities[hash_key]
+        @word_pairs_and_probabilities[hash_key] << hash_value
       else
-        word_pairs_and_probabilities[hash_key] = [hash_value]
+        @word_pairs_and_probabilities[hash_key] = [hash_value]
       end
     end
   end
 
   def speak
-    puts word_pairs_and_probabilities.first
+  	sentence = 0
+  	fragment = @word_pairs_and_probabilities.first[0].split(' ')
+
+  	while sentence < 35 do 
+  		word_pair = fragment.last(2).join(' ')
+  		next_word = @word_pairs_and_probabilities[word_pair].sample unless @word_pairs_and_probabilities[word_pair].nil?
+  		fragment << next_word
+  		
+  		if next_word && (next_word.include?(".") || next_word.include?("?"))
+  			sentence += 1
+  		end
+  	end 
+  	
+  	puts fragment.join(' ')
+
   end
 end
 
 bender = LitBot.new
 bender.eat_file("huckle.txt")
-bender.speak_first_10_words # an example call
 bender.digest_file	
-#bender.speak
+bender.speak
+
+puts
+puts "And now for something entirely different"
+puts
 
 ulysses_bot = LitBot.new
 ulysses_bot.eat_file("ulysses.txt")
-ulysses_bot.speak_first_10_words # an example call
 ulysses_bot.digest_file
-#ulysses_bot.speak
+ulysses_bot.speak
 
 # BONUS HW
-hybrid_bot = ulysses_bot + bender
+#hybrid_bot = ulysses_bot + bender
 #hybrid_bot.speak
 
 
